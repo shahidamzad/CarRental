aea83c0c13c3d3c10c7f5f15218b4a6e4adb2814
shahidamzad <shahidamzad07@gmail.com>
2026-02-27 00:25:31 +0530

    commplete dashboard

--- client/src/pages/Owner/ManageCars.jsx
+++ client/src/pages/Owner/ManageCars.jsx
@@ -20,7 +20,7 @@ const ManageCars = () => {
      
     <div className='px-4 pt-10 md:px-10 w-full '>
 
-      <Title  title='Mange Car'  subTitle= 'View all listed cars , update their details , or remove them from the booking platform ' />
+      <Title  title='Manage Car'  subTitle= 'View all listed cars , update their details , or remove them from the booking platform ' />
 
       <div className='max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6'>
 
