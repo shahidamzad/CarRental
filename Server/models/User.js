973fb3b4d044ab355c320f382af533986e7ed838
shahidamzad <shahidamzad07@gmail.com>
2026-02-27 22:29:48 +0530

    complete register or login api

---
+++ Server/models/User.js
@@ -0,0 +1,36 @@
+
+import mongoose from "mongoose"; 
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ["owner", "user"],
+      default: "user"
+    },
+    image: {
+      type: String,
+      default: ""
+    }
+  },
+  {
+    timestamps: true   // ✅ BOOLEAN (not string)
+  }
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoute.js";
 
 
 // Initialize Express App
@@ -13,7 +14,10 @@ app.use(cors());
 app.use(express.json())
 
 
-app.get('/' , (req , res)=>res.send('server is running  '))
+// Routes
+app.get('/' , (req , res)=>res.send('server is running  '));
+
+app.use('/api/user' , userRouter)
 
 const PORT = process.env.PORT || 3000 ;
 
