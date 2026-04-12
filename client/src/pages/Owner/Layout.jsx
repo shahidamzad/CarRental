c10ff9820cf6046780155e44ae48432a1bcf2a74
shahid hussain <151863732+shahidamzad@users.noreply.github.com>
2026-04-12 22:59:06 +0530

    Fix import path for NavbarOwner component

--- client/src/pages/Owner/Layout.jsx
+++ client/src/pages/Owner/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import NavbarOwner from '../../components/owner/NavbarOwner.jsx'
+import NavbarOwner from '../../components/Owner/NavbarOwner.jsx'
 import Sidebar from '../../components/Owner/Sidebar.jsx'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext.jsx'
